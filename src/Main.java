3e15394ed27ab0d9a73392812986a0ad89ae27ac
saltanat <saltanatnematillakyzy6@gmail.com>
2022-11-07 13:58:12 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] array={"Technical lesson, Technical English"};
+        MyClass myClass = new MyClass(" Saltanat", " Nematilla kyzy", 20,array ," Dymdama");
+
+        System.out.println(" Name:"+myClass.getName());
+        System.out.println(" Surname:"+ myClass.getSurname() );
+        System.out.println(" Age:"+ myClass.getAgef());
+        System.out.println(" Courses that you visit: "+ Arrays.toString(array));
+        System.out.println(" Favorite food: "+myClass.getFood());
+        System.out.println();
+
+        MyClass myClass1 = new MyClass("Nuriza", "Muratova", 20, array, "Manty ");
+        System.out.println("Name:"+myClass1.getName());
+        System.out.println("Surname: "+myClass1.getSurname());
+        System.out.println("Age: "+myClass1.getAgef());
+        System.out.println("Courses that she visit: "+Arrays.toString(array));
+        System.out.println("Favorite food:  "+myClass1.getFood());
+
     }
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class myClass {
+public class MyClass {
+    private String name;
+    private String surname;
+    private int age;
+    private String[] courses;
+    private String food;
+
+    public MyClass(String name, String surname, int age, String [] courses,String dymdama){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.courses = courses;
+        this.food = dymdama;
+
+    }
+    public MyClass (){
+
+    }
+
+
+    void setName(String name){
+        this.name = name;
+    }
+    String getName(){
+        return name;
+    }
+    void setSurname(String surname){
+        this.surname = surname;
+    }
+    String getSurname(){
+        return surname ;
+    }
+    void setAge(int age){
+        this.age = age;
+    }
+    int getAgef(){
+        return age;
+    }
+    void setCourses(String[] courses){
+
+        this.courses = courses;
+    }
+    void setFood(String food){
+        this.food = food;
+    }
+
+    String getFood(){
+        return food;
+    }
 }
